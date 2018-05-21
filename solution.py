79c376a59e887797b2be076d945a58dd83d3294d
circle_path <386763493@qq.com>
2018-05-21 21:45:59 +0800

    在类内部定义了函数

--- solution.py
+++ solution.py
@@ -1,64 +1,89 @@
 class Solution(object):
-    def isMatch(self, s, p):
+
+    def __init__(self):
+        s = input('please input the data of S:\n')
+        p = input('please input the data of P:\n')
+        self.s = s
+        self.p = p
+        self.isMatch()
+
+    def meeting_star(self, s_ptr, p_ptr):
         """
-        :type s: str
-        :type p: str
+        :type s_ptr: int
+        :type p_ptr: int
+        :rtype: (int, int)
+        """
+
+        if p_ptr + 1 >= len(self.p)
+            s_ptr = len(self.s)
+            p_ptr = len(self.p)
+            return s_ptr, p_ptr
+        p_ptr += 1
+
+        while self.p[p_ptr] != '*':
+            if p_ptr >= len(self.p):
+                break
+            p_ptr += 1
+
+        while self.s[s_ptr] != self.p[p_ptr]:
+            s_ptr += 1
+            if s_ptr == len(self.s):
+                s_ptr = False
+                return s_ptr, p_ptr
+
+        s_ptr_copy = s_ptr - 1 
+        p_ptr_copy = p_ptr - 1
+        while True:
+            if self.s[s_ptr_copy] != self.p[p_ptr_copy] and self.p[p_ptr_copy] != '?':
+                break
+
+        return s_ptr, p_ptr
+
+    @staticmethod
+    def meeting_question(s_ptr, p_ptr):
+        """
+        :type s_ptr: int
+        :type p_ptr: int
+        :rtype: (int, int)
+        """
+        s_ptr += 1
+        p_ptr += 1
+        return s_ptr, p_ptr
+
+    def meeting_others(self, s_ptr, p_ptr):
+        """
+        :type s_ptr: int
+        :type p_ptr: int
+        :rtype: (int, int)
+        """
+        if self.s[s_ptr] == self.p[p_ptr]:
+            s_ptr += 1
+            p_ptr += 1
+        else:
+            s_ptr = False
+        return s_ptr, p_ptr
+
+    def isMatch(self):
+        """
         :rtype: bool
         """
         s_ptr = 0
         p_ptr = 0
-        p_ptr_copy = 0
-        while True:
-            if s[s_ptr] != p[p_ptr]:
-                if p[p_ptr] == '?':
-                    s_ptr += 1
-                    p_ptr += 1
-                elif p[p_ptr] == '*':
-                    p_ptr_copy = p_ptr + 1
-                    while True:
-                        if s_ptr == len(s) or p_ptr == len(p) or p_ptr_copy == len(p):
-                            break
-                        if p[p_ptr_copy] != s[s_ptr]:
-                            if p[p_ptr_copy] == '?':
-                                p_ptr_copy += 1
-                                s_ptr += 1
-                            elif p[p_ptr_copy] == '*':
-                                p_ptr = p_ptr_copy
-                                p_ptr_copy += 1
-                            else:
-                                s_ptr += 1                        
-                        else:
-                            while True:
-                                p_ptr_copy += 1
-                                s_ptr += 1
-                                if s_ptr == len(s) or p_ptr == len(p) or p_ptr_copy == len(p):
-                                    break
-                                if p[p_ptr_copy] != s[s_ptr]:
-                                    if p[p_ptr_copy] == '?':
-                                        pass
-                                    elif p[p_ptr_copy] == '*':
-                                        p_ptr = p_ptr_copy
-                                        p_ptr_copy += 1
-                                        break
-                                    else:
-                                        p_ptr_copy = p_ptr + 1
-                                        break                                
-                else:
-                    return False
+        while s_ptr < len(self.s) or p_ptr < len(self.p):
+            if self.p[p_ptr] == "*":
+                s_ptr, p_ptr = self.meeting_star(s_ptr, p_ptr)
+            elif self.p[p_ptr] == "?":
+                s_ptr, p_ptr = self.meeting_question(s_ptr, p_ptr)
             else:
-                s_ptr += 1
-                p_ptr += 1
-            if s_ptr == len(s) and p_ptr == len(p):
-                return True
-            elif s_ptr == len(s) and p_ptr != len(p):
-                return False
-            elif p_ptr == len(p) and s_ptr != len(s):
-                return False
+                s_ptr, p_ptr = self.meeting_others(s_ptr, p_ptr)
+            if s_ptr is False:
+                return False     
+        if s_ptr == len(self.s) and p_ptr == len(self.p):
+            return True
+        else:
+            return False
+
 
 if __name__ == '__main__':
-    start = Solution()
-    while True:
-        data_s = input('please input the data of S: \n')
-        data_p = input('please input the data of P: \n')
-        so_ = start.isMatch(data_s, data_p)
-        print(so_)
+    project = Solution()
+    print(project.isMatch())
